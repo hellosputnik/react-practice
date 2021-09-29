dbbb0d4f358ff079712472349ed3955a9871a94d
hellosputnik <hallosputnik@gmail.com>
2021-09-29 00:52:20 -0700

    Update Clickable component
     * Added constructor with state for location of Clickable
     * Moved location-related code from render to separate function

--- src/Clickable.js
+++ src/Clickable.js
@@ -1,15 +1,36 @@
 import React from 'react';
 
 class Clickable extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      left: 128,
+      top: 128,
+    };
+  }
+
+  handleClick() {
+    this.props.onClick();
+
+    const new_left = getRandomInt(128, 1000);
+    const new_top = getRandomInt(128, 1000);
+
+    this.setState({
+      left: new_left,
+      top: new_top,
+    });
+  }
+
   render() {
     const style = {
       position: 'absolute',
-      left: getRandomInt(0, 1000),
-      top: getRandomInt(0, 1000),
+      left: this.state.left,
+      top: this.state.top,
     }
 
     return (
-      <button onClick={this.props.onClick} style={style}>
+      <button onClick={() => this.handleClick()} style={style}>
         Target
       </button>
     );
